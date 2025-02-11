ea0e5dcd3cdb075effa33a3b1d25763f7ca7401c
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-02-11 22:17:40 +0700

    Initial Commit

---
+++ ecoin/ecoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoinApp.swift
+//  ecoin
+//
+//  Created by Đinh Quốc Việt on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ecoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
